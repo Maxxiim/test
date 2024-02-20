a7cd0cce19b8ba64c4536b3c409cc5c12038ecb8
Maxim <Safronovmaxiim@mail.ru>
2024-02-20 22:28:51 +0300

    part shortby

--- main.js
+++ main.js
@@ -39,3 +39,51 @@ function autoSlider() {
 
 let sliderTimer = 1000 * 5;
 let timerSlider = setInterval(() => autoSlider(), sliderTimer);
+
+// ==================================================================
+// unput range filter price
+
+const rangeInput = document.querySelectorAll('.price-input__slider-range-input input');
+const progress = document.querySelector('.price-input__slider-progress');
+const numberInput = document.querySelectorAll(".price-input__field input");
+let priceGap = 300;
+
+numberInput.forEach((input) => {
+    input.addEventListener('input', (e) => {
+        let minValue = numberInput[0].value;
+        let maxValue = numberInput[1].value;
+
+
+        if ((maxValue - minValue >= priceGap) && maxValue <= 2000) {
+            if (e.target.className === "price-input__field-min") {
+                rangeInput[0].value = minValue;
+                progress.style.left = (minValue / rangeInput[0].max) * 100 + "%";
+
+            } else {
+                rangeInput[1].value = maxValue;
+                progress.style.right = 100 - (maxValue / rangeInput[1].max) * 100 + "%";
+            }
+        };
+    });
+});
+
+rangeInput.forEach((input) => {
+    input.addEventListener('input', (e) => {
+        let minValue = rangeInput[0].value;
+        let maxValue = rangeInput[1].value;
+
+        if (maxValue - minValue <= priceGap) {
+            if (e.target.className === "price-input__slider-range-input-min") {
+                rangeInput[0].value = maxValue - priceGap;
+            } else {
+                rangeInput[1].value = minValue + priceGap;
+            }
+        } else {
+            numberInput[0].value = minValue;
+            numberInput[1].value = maxValue;
+            progress.style.left = (minValue / rangeInput[0].max) * 100 + "%";
+            progress.style.right = 100 - (maxValue / rangeInput[1].max) * 100 + "%";
+        }
+    });
+});
+// ==================================================================
